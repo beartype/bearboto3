d0bbdfcb0039fc3b92fb3b691291b5ff11641101
Paul <dev@studiop.page>
2021-11-13 17:27:59 -0500

    Added __init__.py

---
+++ bearboto3/__init__.py
@@ -0,0 +1,6 @@
+from sys import version_info
+
+MIN_PYTHON_VER = (3,6,2)
+PYTHON_VER_SPLIT = (3,9)
+
+assert version_info >= MIN_PYTHON_VER
--- types.py
+++
@@ -1,5 +0,0 @@
-from typing import Annotated
-from beartype.vale import Is
-from mypy_boto3_s3.service_resource import Bucket
-
-S3Bucket = Annotated[object, lambda obj: obj.__class__.__name__ == "s3.Bucket"]
