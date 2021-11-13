22f7ee23bac273383cde2291fc2aff4ba41644ba
Paul <dev@studiop.page>
2021-11-13 18:59:42 -0500

    Updated pylint settings

--- bearboto3/__init__.py
+++ bearboto3/__init__.py
@@ -1,6 +1,18 @@
+"""Ensures a supported version of python is being used,
+as well as handling appropriate imports based on the current
+python version.
+"""
+
+
 from sys import version_info
 
-MIN_PYTHON_VER = (3,6,2)
-PYTHON_VER_SPLIT = (3,9)
+MIN_PYTHON_VER = (3, 6, 2)
+PYTHON_VER_SPLIT = (3, 9)
 
 assert version_info >= MIN_PYTHON_VER
+
+if version_info >= PYTHON_VER_SPLIT:
+    # pylint: disable=no-name-in-module
+    from typing import Annotated
+else:
+    from typing_extensions import Annotated
