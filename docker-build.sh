f9e6e357d3708561b26b6ee806c4f10da4f21e64
Paul Hutchings <36493057+paulhutchings@users.noreply.github.com>
2021-12-30 23:55:18 +0000

    Delete docker-build.sh

--- docker-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-versions=('3.7' '3.8' '3.9' '3.10')
-
-for version in "${versions[@]}"; do
-image_name="wuubb/bearboto3:$version"
-docker build --tag "$image_name" --build-arg PYTHON_VERSION="$version" .
-docker push "$image_name"
-done
