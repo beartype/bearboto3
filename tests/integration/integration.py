4e48798430505bd9e08313a782ef51fdfc398399
Paul <dev@studiop.page>
2022-02-02 15:29:30 -0500

    Updated integration test

--- tests/integration/integration.py
+++ tests/integration/integration.py
@@ -1,10 +1,16 @@
+import uuid
+
 import boto3
-from bearboto3.s3 import Bucket
+from bearboto3.s3 import Bucket, Object
 
 
-def test_bucket(bucket: Bucket):
-    pass
+def random_str():
+    return str(uuid.uuid4())
 
-s3 = boto3.resource('s3')
 
-test_bucket(s3.Bucket('foo'))
+def integration(bucket: Bucket) -> Object:
+    return bucket.Object(random_str())
+
+
+s3 = boto3.resource("s3")
+obj = integration(s3.Bucket(random_str()))
